155fbc7b8ca747757f94c68f6abb44dce0622628
progyu <rocket7101@gmail.com>
2019-05-19 12:51:00 +0900

    Update: algorithm

--- algorithm/20190517.js
+++ algorithm/20190517.js
@@ -1,43 +1,38 @@
-// 10. 배열의 최대 / 최소값 구하기
-// 배열의 요소 중 최대값 / 최소값을 반환하는 함수를 완성하라.
-
-function getMaxValueFromArray(array) {
-    // return Math.max.apply(null, array);
-    return Math.max(...array); // ES6
-}
-console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
-
-function getMinValueFromArray(array) {
-    return Math.min.apply(null, array);
-}
-console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
-
 
 // 4. 문자열 내 p와 y의 개수
-// numPY함수는 대문자와 소문자가 섞여있는 문자열 s를 인수로 전달받는다. s에 존재하는 'p'의 개수와 'y'의 갯수를 비교해 같으면 true, 
+// numPY함수는 대문자와 소문자가 섞여있는 문자열 s를 인수로 전달받는다. s에 존재하는 'p'의 개수와 'y'의 갯수를 비교해 같으면 true,
 // 다르면 false를 리턴하도록 함수를 완성하라. 대소문자를 구별하지 않으며 'p', 'y' 모두 하나도 없는 경우는 항상 true를 리턴한다.
 
 // 예를 들어 s가 'pPoooyY'면 true를 리턴하고 'Pyy'라면 false를 리턴한다.
 
 
 function numPY(s) {
-    const str = s ? s.toUpperCase() : '';
-    let cntP = 0;
-    let cntY = 0;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === 'P') cntP++;
-        if (str[i] === 'Y') cntY++;
-    }
-
-    return cntP === cntY;
+  const str = s ? s.toUpperCase() : '';
+  const arr = Array.from(str);
+  const filterP = arr.filter(item => item === 'P');
+  const filterY = arr.filter(item => item === 'Y');
+  return filterP.length === filterY.length;
 }
 
+
+// function numPY(s) {
+//   const str = s ? s.toUpperCase() : '';
+//   let cntP = 0;
+//   let cntY = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === 'P') cntP += 1;
+//     if (str[i] === 'Y') cntY += 1;
+//   }
+
+//   return cntP === cntY;
+// }
+
 console.log(numPY('pPoooyY')); // true
-console.log(numPY('Pyy'));     // false
-console.log(numPY('ab'));      // true
-console.log(numPY(''));        // true
-console.log(numPY());          // true
+console.log(numPY('Pyy')); // false
+console.log(numPY('ab')); // true
+console.log(numPY('')); // true
+console.log(numPY()); // true
 
 
 // 5. 이상한 문자만들기
@@ -47,45 +42,43 @@ console.log(numPY());          // true
 
 // 주의) 문자열 전체의 짝/홀수 인덱스가 아니라 단어(공백을 기준)별로 짝/홀수 인덱스를 판단한다.
 function toWeirdCase(s) {
-    // 'hello world' => ['hello', 'world'];
-    const wordArr = s.split(' ');
+  // 'hello world' => ['hello', 'world'];
+  const wordArr = s.split(' ');
 
-    function toUpperLower(str) {
-        let res = '';
-        for (let i = 0; i < str.length; i++) {
-            // 0 이면 짝수 , 아니면 홀수
-            res += i % 2 ? str[i].toLowerCase() : str[i].toUpperCase();
-        }
-        return res;
+  function toUpperLower(str) {
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+      // 0 이면 짝수 , 아니면 홀수
+      res += i % 2 ? str[i].toLowerCase() : str[i].toUpperCase();
     }
-    // console.log(wordArr);
-    for (let i = 0; i < wordArr.length; i++) {
-        wordArr[i] = toUpperLower(wordArr[i]);
-        // console.log(toUpperLower(wordArr[i]));
-    }
-    // console.log(wordArr);
-    return wordArr.join(' ');
+    return res;
+  }
+  // console.log(wordArr);
+  for (let i = 0; i < wordArr.length; i++) {
+    wordArr[i] = toUpperLower(wordArr[i]);
+    // console.log(toUpperLower(wordArr[i]));
+  }
+  // console.log(wordArr);
+  return wordArr.join(' ');
 }
 
-console.log(toWeirdCase('hello world'));    // 'HeLlO WoRlD'
+console.log(toWeirdCase('hello world')); // 'HeLlO WoRlD'
 console.log(toWeirdCase('my name is lee')); // 'My NaMe Is LeE'
 
 
-
 // 6. 핸드폰번호 가리기
-// 핸드폰 요금 고지서에 표시할 전화번호는 개인정보 보호를 위해 맨 뒷자리 4자리를 제외한 나머지를 *으로 바꿔야 한다. 
-// 전화번호를 나타내는 문자열 str을 입력받는 hideNumbers 함수를 완성하라 
+// 핸드폰 요금 고지서에 표시할 전화번호는 개인정보 보호를 위해 맨 뒷자리 4자리를 제외한 나머지를 *으로 바꿔야 한다.
+// 전화번호를 나타내는 문자열 str을 입력받는 hideNumbers 함수를 완성하라
 // 예를들어 s가 ‘01033334444’면 *******4444를 리턴하고, ‘027778888’인 경우는 *****8888을 리턴한다.
 
 function hideNumbers(str) {
-    const cut = str.slice(-4);
-    const star = '*'.repeat(str.length - 4);
-    return star.concat(cut);
+  const cut = str.slice(-4);
+  const star = '*'.repeat(str.length - 4);
+  return star.concat(cut);
 }
 
 console.log(hideNumbers('01033334444')); // *******4444
-console.log(hideNumbers('027778888'));   // *****8888
-
+console.log(hideNumbers('027778888')); // *****8888
 
 
 // 7. 문자열을 숫자로 바꾸기
@@ -94,13 +87,13 @@ console.log(hideNumbers('027778888'));   // *****8888
 // 예를 들어 str이 ‘1234’이면 1234를 반환하고, ‘-1234’이면 -1234를 반환한다. str은 부호(+,-)와 숫자로만 구성되어 있고, 잘못된 값이 입력되는 경우는 없다.
 
 function strToInt(str) {
-    return +str;
-    // return str * 1;
-    // return Number(str);
-    // return parseInt(str);
+  return +str;
+  // return str * 1;
+  // return Number(str);
+  // return parseInt(str);
 }
 
-console.log(strToInt('1234'));  // 1234
+console.log(strToInt('1234')); // 1234
 console.log(strToInt('-1234')); // -1234
 
 
@@ -110,16 +103,16 @@ console.log(strToInt('-1234')); // -1234
 // 예를 들어 n이 4이면 ‘수박수박’을 리턴하고 3이라면 ‘수박수’를 리턴한다.
 
 function waterMelon(n) {
-    let str = '';
-    for (i = 0; i < n; i++) {
-        if (i % 2) str += '박'
-        else str += '수';
-    }
-    return str;
+  let str = '';
+  for (let i = 0; i < n; i++) {
+    if (i % 2) str += '박';
+    else str += '수';
+  }
+  return str;
 }
 
-console.log('n이 3인 경우: ' + waterMelon(3));
-console.log('n이 4인 경우: ' + waterMelon(4));
+console.log(`n이 3인 경우: ${waterMelon(3)}`);
+console.log(`n이 4인 경우: ${waterMelon(4)}`);
 
 
 // 9. 정수제곱근 판별하기
@@ -128,15 +121,30 @@ console.log('n이 4인 경우: ' + waterMelon(4));
 // 예를 들어 n이 121이라면 이는 정수 11의 제곱이므로 (11+1)의 제곱인 144를 리턴하고, 3이라면 ‘no’을 리턴한다.
 
 function nextSqaure(n) {
-    const squareRoot = Math.sqrt(n);
-    return Number.isInteger(squareRoot) ? Math.pow(squareRoot + 1, 2) : 'no';
+  const squareRoot = Math.sqrt(n);
+  return Number.isInteger(squareRoot) ? (squareRoot + 1) ** 2 : 'no';
 }
 
-console.log(nextSqaure());    // no
-console.log(nextSqaure(0));   // 1
-console.log(nextSqaure(1));   // 4
-console.log(nextSqaure(2));   // no
-console.log(nextSqaure(3));   // no
+console.log(nextSqaure()); // no
+console.log(nextSqaure(0)); // 1
+console.log(nextSqaure(1)); // 4
+console.log(nextSqaure(2)); // no
+console.log(nextSqaure(3)); // no
 console.log(nextSqaure(121)); // 144
 console.log(nextSqaure(165)); // no
 console.log(nextSqaure(400)); // 441
+
+
+// 10. 배열의 최대 / 최소값 구하기
+// 배열의 요소 중 최대값 / 최소값을 반환하는 함수를 완성하라.
+
+function getMaxValueFromArray(array) {
+  // return Math.max.apply(null, array);
+  return Math.max(...array); // ES6
+}
+console.log(getMaxValueFromArray([3, 6, -2, -5, 7, 3])); // 7
+
+function getMinValueFromArray(array) {
+  return Math.min.apply(null, array);
+}
+console.log(getMinValueFromArray([3, 6, -2, -5, 7, 3])); // -5
